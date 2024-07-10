cfcb09293057d53c98012a84cff1cfa4dffbdaa7
Sean Marpo <seanmarpo@users.noreply.github.com>
2024-07-09 23:32:41 -0700

    Add support for URL sharing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import CodeMirror from 'codemirror';
 import * as Diff from 'diff';
 import * as Diff2Html from 'diff2html/lib/ui/js/diff2html-ui';
 
+import lzutf8 from 'lzutf8';
+
 import 'codemirror/lib/codemirror.css';
 import 'diff2html/bundles/css/diff2html.min.css';
 import './index.css';
@@ -31,6 +33,20 @@ const updateText = () => {
 };
 
 const render = () => {
+  if (lEditor.getValue() != '' || rEditor.getValue() != '') {
+    const leftCompressed = lzutf8.compress(lEditor.getValue(), {outputEncoding: 'Base64'});
+    const rightCompressed = lzutf8.compress(rEditor.getValue(), {outputEncoding: 'Base64'});
+    window.location.hash = `${leftCompressed}|${rightCompressed}`;
+  } else if (window.location.hash != '') {
+    const [lCompressed, rCompressed] = window.location.hash.split('#')[1].split('|');
+    console.log(`left = ${lCompressed} | right = ${rCompressed}`);
+    const lDecompressed = lzutf8.decompress(lCompressed, {inputEncoding: 'Base64'});
+    const rDecompressed = lzutf8.decompress(rCompressed, {inputEncoding: 'Base64'});
+    lEditor.setValue(lDecompressed);
+    rEditor.setValue(rDecompressed);
+    updateText();
+  }
+  
   new Diff2Html.Diff2HtmlUI(elemDiff,
     Diff.createPatch('diff', lText, rText),
     {
